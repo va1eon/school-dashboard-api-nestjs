9858bef933e3f8377cce942e4518990d736cb09b
Valery Mikhailov <va1eonmix@gmail.com>
2026-01-29 09:37:00 +0700

    feat(config): add example env, docker compose

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
 	{
-		ignores: ['eslint.config.mjs', 'prettier.config.mjs'],
+		ignores: ['eslint.config.mjs', 'prettier.config.mjs', 'prisma'],
 	},
 	eslint.configs.recommended,
 	...tseslint.configs.recommendedTypeChecked,
